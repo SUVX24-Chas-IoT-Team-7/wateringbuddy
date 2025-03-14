0ccf6d0ac27f760964a939ad89f8a3f5584d0915
Love Lindeborg <lindeborglove@gmail.com>
2025-03-14 11:47:35 +0100

    Created and Implemented moistureSensor class

---
+++ headers/MoistureSensor.hpp
@@ -0,0 +1,54 @@
+/**
+ * @file MoistureSensor.hpp
+ * @author Love Lindeborg (lindeborglove@gmail.com)
+ * @brief Collects information from moisture sensor on a specified pin.
+ * @version 0.1
+ * @date 2025-03-14
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
+ #ifndef __MOISTURE_SENSOR_CLASS_HPP__
+ #define __MOISTURE_SENSOR_CLASS_HPP__
+ 
+ #include <stdint.h>
+ #include "Pin.hpp"
+ #include "Sensor.hpp"
+ 
+ /**
+  * @brief This class collects data from a moisture sensor at a specified pin.
+  * 
+  */
+ class MoistureSensor : public Sensor 
+ {
+ public:
+
+    struct LedPins {
+        uint8_t green, yellow, red, blue;
+    };
+
+    /**
+     * @brief Construct a new MoistureSensor object
+     * 
+     * @param pin Which pin to read data from.
+     */
+    MoistureSensor(uint8_t powerPin, uint8_t inputPin, LedPins ledPins, int thresholds[3]);
+
+    ~MoistureSensor() = default;
+
+    /**
+     * @brief Sets pin from constructor to INPUT using ´pinMode´ and 
+     */
+    void init() override;
+ 
+ private:
+    uint8_t m_powerPin;
+    LedPins m_ledPins;
+
+    int thresholds[3];
+    float percentage = 0;
+
+ };
+ 
+ #endif
---
+++ src/MoistureSensor.cpp
@@ -0,0 +1,20 @@
+#include "MoistureSensor.hpp"
+
+MoistureSensor::MoistureSensor(uint8_t powerPin, uint8_t inputPin, LedPins ledPins, int thresholds[3])
+    : Sensor(inputPin), m_powerPin(powerPin), m_ledPins(ledPins)
+{
+    this->thresholds[0] = thresholds[0];
+    this->thresholds[1] = thresholds[1];
+    this->thresholds[2] = thresholds[2];
+}
+
+void MoistureSensor::init() 
+{
+    pinMode(m_powerPin, OUTPUT);
+    
+    for (uint8_t *i = (uint8_t *) &m_ledPins.green; (uint32_t) i < (uint32_t) &m_ledPins.blue; i++) {
+        pinMode(*i, OUTPUT);
+    }
+
+    pinMode(this->m_pinNumber, INPUT);
+}
