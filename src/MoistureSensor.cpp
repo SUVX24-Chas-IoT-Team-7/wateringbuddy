a6b455c2445785ac5f4192ce58fe6872d817e889
Love Lindeborg <lindeborglove@gmail.com>
2025-03-14 11:49:36 +0100

    Update MoistureSensor.cpp

--- src/MoistureSensor.cpp
+++ src/MoistureSensor.cpp
@@ -12,7 +12,7 @@ void MoistureSensor::init()
 {
     pinMode(m_powerPin, OUTPUT);
     
-    for (uint8_t *i = (uint8_t *) &m_ledPins.green; (uint32_t) i < (uint32_t) &m_ledPins.blue; i++) {
+    for (uint8_t *i = &m_ledPins.green; (uint32_t) i < (uint32_t) &m_ledPins.blue; i++) {
         pinMode(*i, OUTPUT);
     }
 
