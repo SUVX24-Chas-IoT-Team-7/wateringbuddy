9b529e3fcad763df0628c3d391337b911d6670ff
Erik Dahl <erik@iunderlandet.se>
2025-03-17 14:50:58 +0100

    update ThresholdManager

--- headers/ThresholdManager.hpp
+++ headers/ThresholdManager.hpp
@@ -8,170 +8,100 @@
  * It also has methods to get the current threshold value and name
  * It has methods to get the moisture status and LED pin based on the moisture value
  */
-class ThresholdManager
+
+#include "ConstantsAndEnums.hpp"
+#include <algorithm>
+
+ class ThresholdManager
 {
 private:
-    int veryWetThreshold = 200;
-    int wetThreshold = 400;
-    int moistThreshold = 600;
-    int dryThreshold = 800;
-
-    int currentThreshold = 0;
-
-    int mode = 0;
+    int m_moistureCenter;
+    int m_thresholds[3];
 
 public:
-    ThresholdManager() {}
-
-    void toggleMode()
-    {
-        mode = !mode;
-        currentThreshold = 0;
-    }
-
-    int getMode() const
-    {
-        return mode;
-    }
-
-    void nextThreshold()
-    {
-        currentThreshold = (currentThreshold + 1) % 4;
-    }
-
-    const char *getCurrentThresholdName() const
-    {
-        switch (currentThreshold)
+    ThresholdManager(int moistureCenter = 60) 
+        : m_moistureCenter { moistureCenter } 
         {
-        case 0:
-            return "Very Wet";
-        case 1:
-            return "Wet";
-        case 2:
-            return "Moist";
-        case 3:
-            return "Dry";
-        default:
-            return "Unknown";
+            setNewThresholds(m_moistureCenter);
+        }
+
+    /**
+     * @brief Set Thresholds values
+     * 
+     * @param newMoistureCenter accepted values 35-85
+     */
+    void setNewThresholds(int newMoistureCenter) {
+        if (newMoistureCenter >= 35 && newMoistureCenter <= 85 ) {
+            m_thresholds[DRY] =  std::max(16, (int)(newMoistureCenter * 0.42)); 
+            m_thresholds[GOOD] = newMoistureCenter - 10;
+            m_thresholds[WET] = std::min(90, newMoistureCenter + 10);
         }
     }
 
-    int getCurrentThresholdValue() const
+
+    int getMoistureCenterValue() const
     {
-        switch (currentThreshold)
-        {
-        case 0:
-            return veryWetThreshold;
-        case 1:
-            return wetThreshold;
-        case 2:
-            return moistThreshold;
-        case 3:
-            return dryThreshold;
-        default:
-            return 0;
+        return m_moistureCenter;
+    }
+
+    void increaseThreshold()
+    {
+
+        if (m_moistureCenter < 85) {
+            // update center value
+            m_moistureCenter++;
+            // update other threshold values
+            setNewThresholds(m_moistureCenter);
         }
     }
 
-    void increaseThreshold(int amount = 10)
+    void decreaseThreshold()
     {
-        switch (currentThreshold)
-        {
-        case 0:
-            veryWetThreshold += amount;
-            if (veryWetThreshold >= wetThreshold)
-                veryWetThreshold = wetThreshold - 10;
-            break;
-        case 1:
-            wetThreshold += amount;
-            if (wetThreshold >= moistThreshold)
-                wetThreshold = moistThreshold - 10;
-            break;
-        case 2:
-            moistThreshold += amount;
-            if (moistThreshold >= dryThreshold)
-                moistThreshold = dryThreshold - 10;
-            break;
-        case 3:
-            dryThreshold += amount;
-            if (dryThreshold > 1023)
-                dryThreshold = 1023;
-            break;
+        if (m_moistureCenter > 35) {
+            // update center value
+            m_moistureCenter--;
+            // update other threshold values
+            setNewThresholds(m_moistureCenter);
         }
     }
 
-    void decreaseThreshold(int amount = 10)
+    const char *getMoistureString(int moistureValue) const
     {
-        switch (currentThreshold)
-        {
-        case 0:
-            veryWetThreshold -= amount;
-            if (veryWetThreshold < 0)
-                veryWetThreshold = 0;
-            break;
-        case 1:
-            wetThreshold -= amount;
-            if (wetThreshold <= veryWetThreshold)
-                wetThreshold = veryWetThreshold + 10;
-            break;
-        case 2:
-            moistThreshold -= amount;
-            if (moistThreshold <= wetThreshold)
-                moistThreshold = wetThreshold + 10;
-            break;
-        case 3:
-            dryThreshold -= amount;
-            if (dryThreshold <= moistThreshold)
-                dryThreshold = moistThreshold + 10;
-            break;
-        }
-    }
-
-    const char *getMoistureStatus(int moistureValue) const
-    {
-        if (moistureValue < veryWetThreshold)
-        {
-            return "Very Wet";
-        }
-        else if (moistureValue < wetThreshold)
-        {
-            return "Wet";
-        }
-        else if (moistureValue < moistThreshold)
-        {
-            return "Moist";
-        }
-        else if (moistureValue < dryThreshold)
+        if (moistureValue < m_thresholds[DRY])
         {
             return "Dry";
+        }
+        else if (moistureValue < m_thresholds[GOOD])
+        {
+            return "Semidry";
+        }
+        else if (moistureValue < m_thresholds[WET])
+        {
+            return "Good!";
         }
         else
         {
-            return "Very Dry";
+            return "Wet :-(";
         }
     }
 
     int getLEDPin(int moistureValue) const
     {
-        if (moistureValue < veryWetThreshold)
+        if (moistureValue < m_thresholds[DRY])
         {
-            return 12;
+            return moisture::redPin;
         }
-        else if (moistureValue < wetThreshold)
+        else if (moistureValue < m_thresholds[GOOD])
         {
-            return 11;
+            return moisture::yellowPin;
         }
-        else if (moistureValue < moistThreshold)
+        else if (moistureValue < m_thresholds[WET])
         {
-            return 10;
-        }
-        else if (moistureValue < dryThreshold)
-        {
-            return 9;
+            return moisture::greenPin;
         }
         else
         {
-            return 8;
+            return moisture::bluePin;
         }
     }
 };
