03041c5855b6793c07f3f946be23c81b55c44e20
Erik Dahl <erik@iunderlandet.se>
2025-03-17 09:59:35 +0100

    add comments to Buttons.hpp

--- headers/Buttons.hpp
+++ headers/Buttons.hpp
@@ -20,6 +20,13 @@ enum ButtonState {
     LONGPRESS
 };
 
+/**
+ * @brief Handles button presses with functionality for short press and long press
+ * 
+ * Button is assumed to be connected with a pull-down resistor
+ * meaning Button pressed is HIGH and Button open is LOW
+ */
+
 class Button {
 private:
     Pin m_pin;
@@ -30,11 +37,33 @@ private:
     time_t m_pressTime { 0 };
     bool m_isPressed { false };
     bool m_awaitingLongpress { false };
+    /**
+     * @brief Helper function to make functionality explicit
+     * 
+     * @return true when press is detected 
+     * @return false when no press is detected
+     */
     bool pressDetected();
     bool releaseDetected();
 public:
+    /**
+     * @brief Construct a new Button object
+     * 
+     * @param pin Number of the pin the button is connected to
+     */
     Button(int pin, PinMode);
+    /**
+     * @brief Run in setup() to set correct pinMode
+     * 
+     */
     void init();
+    /**
+     * @brief Get the current button state
+     * 
+     * @return ButtonState Enum has the values NOPRESS, SHORTPRESS and LONGPRESS
+     * on release SHORTPRESS is returned if press duration is shorter than m_longPressTime
+     * if a press lasts longer than m_longPressTime LONGPRESS is immediately returned
+     */
     ButtonState getState();
 };
 
