7e6f66eeabc699fc9c93af47edaac43b6eb1f697
Erik Dahl <erik@iunderlandet.se>
2025-03-19 11:13:02 +0100

    implement processToggleButton

--- src/PageController.cpp
+++ src/PageController.cpp
@@ -1,10 +1,51 @@
 #include "PageController.hpp"
 
-
 void PageController::processToggleButton(){
+
     ButtonState currentState = m_buttonToggleMode.getState();
+    // Active mode reset implementation goes here
+
     switch(m_currentMode) {
-        
+        case MOISTURE_DISPLAY:
+        if (currentState == SHORTPRESS) {
+            m_currentMode = LIGHT_DISPLAY;  // go to page 1
+        }
+        else if (currentState == LONGPRESS) {
+            m_currentMode = WATERING_DISPLAY;
+        }
+        break;
+
+        case LIGHT_DISPLAY:
+        if (currentState == SHORTPRESS) {
+            m_currentMode = MOISTURE_TRESHOLD_DISPLAY;  // go to page 2
+        }
+        else if (currentState == LONGPRESS) {
+            // No implementation yet
+        }
+        break;
+
+        case MOISTURE_TRESHOLD_DISPLAY:
+        if (currentState == SHORTPRESS) {
+            m_currentMode = MOISTURE_DISPLAY;           // go to page 0
+        }
+        else if (currentState == LONGPRESS) {
+            m_currentMode = ADJUST_MOISTURE_DISPLAY;
+        }
+        break;
+
+        case WATERING_DISPLAY:
+        if (currentState == SHORTPRESS) {
+            m_currentMode = MOISTURE_DISPLAY;
+        }
+        break;
+
+        case ADJUST_MOISTURE_DISPLAY:
+        if (currentState == SHORTPRESS) {
+            m_currentMode = MOISTURE_TRESHOLD_DISPLAY;
+        }
+
+        break;
+
     }    
 }
 bool PageController::decrementIsPressed(){
