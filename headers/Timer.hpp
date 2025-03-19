f254c414a5063b1308b1831d2b32b75f41847777
John <john.collinder@gmail.com>
2025-03-19 12:04:25 +0100

    Skapat en timer hpp med class

---
+++ headers/Timer.hpp
@@ -0,0 +1,18 @@
+#ifndef TIME_SCHEDULER_HPP
+#define TIME_SCHEDULER_HPP
+#include <PageController.hpp>
+// Skapa en timer för att stänga av LCD efter 10 sekunder
+// Skapa en timer åt WATERING_DISPLAY så att den uppdateras konstant medans dem andra sidorna inte gör det.
+class Scheduler
+{
+private:
+    /* data */
+public:
+    Scheduler(/* args */);
+};
+
+Scheduler::Scheduler(/* args */)
+{
+}
+
+#endif // TIME_SCHEDULER_HPP
